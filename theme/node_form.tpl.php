f3206bef5dc83318ed9cca3a8a181ae50eda240e
Young Hahn <yfhahn@gmail.com>
2009-09-28 10:34:37 -0400

    Removing deprecated files, cleaning up user form.

--- theme/node_form.tpl.php
+++ theme/node_form.tpl.php
@@ -15,8 +15,4 @@
   <div class='buttons clear-block'><?php print drupal_render($buttons) ?></div>
 <?php endif; ?>
 
-<?php if ($hidden): ?>
-  <div class='hidden' style='display:none'><?php print drupal_render($hidden) ?></div>
-<?php endif; ?>
-
 </div>
--- theme/user_form.tpl.php
+++ theme/user_form.tpl.php
@@ -1,10 +1,10 @@
-<div class='form clear-block <?php if (!empty($form_classes)) print $form_classes ?>'>
+<div class='form clear-block <?php print $form_classes ?>'>
+
+<div class='main'><?php print drupal_render($form) ?></div>
 
 <?php if (!empty($sidebar)): ?>
   <div class='sidebar'><?php print drupal_render($sidebar) ?></div>
 <?php endif; ?>
-
-<div class='main'><?php print drupal_render($form) ?></div>
 
 <?php if ($buttons): ?>
   <div class='buttons clear-block'><?php print drupal_render($buttons) ?></div>
